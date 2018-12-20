a77803e0a4f473fd5b473f04203bcb0a5eba23e4
Akash Mahajan <akash7190@gmail.com>
2018-12-19 20:04:33 -0800

    [check_res_dec] fixing WER generating script - didn't seem to work in my case

--- check_res_dec.sh
+++ check_res_dec.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-for x in $1; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep WER $x/wer_* 2>/dev/null | ./best_wer.sh; done
-for x in $1; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep Sum $x/score_*/*.sys 2>/dev/null | ./best_wer.sh; done
+
+# Compiles best WERs from decodings generated in passed dirs (exp dir containing decode_*)
+for d in $@; do for ddecode in $d/decode_*; do echo $ddecode & grep Sum $ddecode/*scor*/*ys | ./best_wer.sh; done; done
 exit 0
-
-
-
 
