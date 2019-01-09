30754e3d118afcbfd482d70670ba32637940f583
Mirco Ravanelli <mirco.ravanelli@gmail.com>
2019-01-08 19:04:25 -0500

    Update neural_networks.py
    Fixing super() issue in SincConv

--- neural_networks.py
+++ neural_networks.py
@@ -1409,7 +1409,7 @@ class SincConv(nn.Module):
                  stride=1, padding=0, dilation=1, bias=False, groups=1,
                  sample_rate=16000, min_low_hz=50, min_band_hz=50):
 
-        super().__init__()
+        super(SincConv,self).__init__()
 
         if in_channels != 1:
             #msg = (f'SincConv only support one input channel '
