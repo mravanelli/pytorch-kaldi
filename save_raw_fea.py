5e3cddf5bb6d10c7bddc7cfc86dc81106136ac42
Parcollet Titouan <titouan.parcollet@alumni.univ-avignon.fr>
2019-03-14 09:18:02 +0100

    Fixing production errors

--- save_raw_fea.py
+++ save_raw_fea.py
@@ -19,11 +19,11 @@ from data_io import read_vec_int_ark,write_mat
 
 # Run it for all the data chunks (e.g., train, dev, test) => uncomment
 
-lab_folder='quick_test/dnn4_pretrain-dbn_dnn_ali'
+lab_folder='/users/parcollet/KALDI/kaldi-trunk/egs/timit/s5/exp/dnn4_pretrain-dbn_dnn_ali_test'
 lab_opts='ali-to-pdf'
-out_folder='raw_TIMIT_200ms/train'
-wav_lst='/home/mirco/pytorch-kaldi-new/quick_test/data/train/wav_lst.scp'
-scp_file_out='quick_test/data/train/feats_raw.scp'
+out_folder='/users/parcollet/KALDI/kaldi-trunk/egs/timit/s5/data/raw_TIMIT_200ms/test'
+wav_lst='/users/parcollet/KALDI/kaldi-trunk/egs/timit/s5/data/test/wav.lst'
+scp_file_out='/users/parcollet/KALDI/kaldi-trunk/egs/timit/s5/data/raw_TIMIT_200ms/test/feats_raw.scp'
 
 #lab_folder='quick_test/dnn4_pretrain-dbn_dnn_ali_dev'
 #lab_opts='ali-to-pdf'
@@ -62,7 +62,7 @@ except:
 scp_file = open(scp_file_out,"w") 
 
 # reading the labels
-lab= { k:v for k,v in read_vec_int_ark('gunzip -c '+lab_folder+'/ali*.gz | '+lab_opts+' '+lab_folder+'/final.mdl ark:- ark:-|')} 
+lab= { k:v for k,v in read_vec_int_ark('gunzip -c '+lab_folder+'/ali*.gz | '+lab_opts+' '+lab_folder+'/final.mdl ark:- ark:-|', out_folder)} 
 
 # reading the list file
 with open(wav_lst) as f:
@@ -107,7 +107,7 @@ for sig_file in sig_lst:
     
     # Save the matrix into a kaldi ark
     out_file=out_folder+'/'+sig_id+'.ark'
-    write_mat(out_file, frame_all, key=sig_id)
+    write_mat(out_folder, out_file, frame_all, key=sig_id)
     print(sig_id)
     scp_file.write(sig_id+' '+out_folder+'/'+sig_id+'.ark:'+str(len(sig_id)+1)+'\n')
 
