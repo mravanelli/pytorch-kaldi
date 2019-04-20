999c34e7fe17c90599cc993af263a1c2217c5f20
Lu Huang <huanglu.th@gmail.com>
2019-04-21 00:01:03 +0800

    Add class SRU

--- neural_networks.py
+++ neural_networks.py
@@ -13,6 +13,10 @@ import numpy as np
 from distutils.util import strtobool
 import math
 
+# uncomment below if you want to use SRU
+# and you need to install SRU: pip install sru[cuda].
+# or you can install it from source code: https://github.com/taolei87/sru.
+# import sru
 
 class LayerNorm(nn.Module):
 
@@ -1657,6 +1661,49 @@ def flip(x, dim):
     x = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1, -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
     return x.view(xsize)
 
+class SRU(nn.Module):
+    def __init__(self, options, inp_dim):
+        super(SRU, self).__init__()
+        self.input_dim = inp_dim
+        self.hidden_size = int(options['sru_hidden_size'])
+        self.num_layers = int(options['sru_num_layers'])
+        self.dropout = float(options['sru_dropout'])
+        self.rnn_dropout = float(options['sru_rnn_dropout'])
+        self.use_tanh = bool(strtobool(options['sru_use_tanh']))
+        self.use_relu = bool(strtobool(options['sru_use_relu']))
+        self.use_selu = bool(strtobool(options['sru_use_selu']))
+        self.weight_norm = bool(strtobool(options['sru_weight_norm']))
+        self.layer_norm = bool(strtobool(options['sru_layer_norm']))
+        self.bidirectional = bool(strtobool(options['sru_bidirectional']))
+        self.is_input_normalized = bool(strtobool(options['sru_is_input_normalized']))
+        self.has_skip_term = bool(strtobool(options['sru_has_skip_term']))
+        self.rescale = bool(strtobool(options['sru_rescale']))
+        self.highway_bias = float(options['sru_highway_bias'])
+        self.n_proj = int(options['sru_n_proj'])
+        self.sru = sru.SRU(self.input_dim, self.hidden_size,
+                            num_layers=self.num_layers,
+                            dropout=self.dropout,
+                            rnn_dropout=self.rnn_dropout,
+                            bidirectional=self.bidirectional,
+                            n_proj=self.n_proj,
+                            use_tanh=self.use_tanh,
+                            use_selu=self.use_selu,
+                            use_relu=self.use_relu,
+                            weight_norm=self.weight_norm,
+                            layer_norm=self.layer_norm,
+                            has_skip_term=self.has_skip_term,
+                            is_input_normalized=self.is_input_normalized,
+                            highway_bias=self.highway_bias,
+                            rescale=self.rescale)
+        self.out_dim = self.hidden_size+self.bidirectional*self.hidden_size
 
-
+    def forward(self, x):
+        if self.bidirectional:
+            h0 = torch.zeros(self.num_layers, x.shape[1], self.hidden_size*2)
+        else:
+            h0 = torch.zeros(self.num_layers, x.shape[1], self.hidden_size)
+        if x.is_cuda:
+            h0 = h0.cuda()
+        output, hn = self.sru(x, c0=h0)
+        return output
 
