8329366839170d0ba7016126c73fcf181791771e
Dmitriy Serdyuk <serdyuk.dmitriy@gmail.com>
2019-05-03 11:31:55 -0400

    Refactor tune hyperparameters

--- tune_hyperparameters.py
+++ tune_hyperparameters.py
@@ -35,62 +35,60 @@ if __name__ == '__main__':
 
         cfg_file_out = output_folder + '/exp' + str(i) + '.cfg'
 
-        cfg_out = open(cfg_file_out, 'w')
+        with open(cfg_file_out, 'wt') as cfg_out, \
+             open(cfg_file, 'rt') as cfg_in:
+            for line in cfg_in:
 
-        for line in open(cfg_file):
+                key = line.split('=')[0]
 
-            key = line.split('=')[0]
+                if key == 'out_folder':
+                    line = 'out_folder=' + output_folder + '/exp' + str(i) + '\n'
 
-            if key == 'out_folder':
-                line = 'out_folder=' + output_folder + '/exp' + str(i) + '\n'
+                hyper_found = False
+                for hyperparam in hyperparam_list:
 
-            hyper_found = False
-            for hyperparam in hyperparam_list:
+                    key_hyper = hyperparam.split('=')[0]
 
-                key_hyper = hyperparam.split('=')[0]
+                    if key == key_hyper:
 
-                if key == key_hyper:
+                        if "randint" in hyperparam:
+                            [lower, higher] = re.search('randint\((.+?)\)',
+                                                        hyperparam).group(1).split(
+                                ',')
+                            value_hyper = randint(int(lower), int(higher))
+                            hyper_found = True
 
-                    if "randint" in hyperparam:
-                        [lower, higher] = re.search('randint\((.+?)\)',
-                                                    hyperparam).group(1).split(
-                            ',')
-                        value_hyper = randint(int(lower), int(higher))
-                        hyper_found = True
+                        if "randfloat" in hyperparam:
+                            [lower, higher] = re.search('randfloat\((.+?)\)',
+                                                        hyperparam).group(1).split(
+                                ',')
+                            value_hyper = random.uniform(float(lower),
+                                                         float(higher))
+                            hyper_found = True
 
-                    if "randfloat" in hyperparam:
-                        [lower, higher] = re.search('randfloat\((.+?)\)',
-                                                    hyperparam).group(1).split(
-                            ',')
-                        value_hyper = random.uniform(float(lower),
-                                                     float(higher))
-                        hyper_found = True
+                        if "choose_str" in hyperparam:
+                            value_hyper = random.choice(
+                                re.search('\{(.+?)\}', hyperparam).group(1).split(
+                                    '|'))
+                            hyper_found = True
 
-                    if "choose_str" in hyperparam:
-                        value_hyper = random.choice(
-                            re.search('\{(.+?)\}', hyperparam).group(1).split(
-                                '|'))
-                        hyper_found = True
+                        if "choose_int" in hyperparam:
+                            value_hyper = int(random.choice(
+                                re.search('\{(.+?)\}', hyperparam).group(1).split(
+                                    '|')))
+                            hyper_found = True
 
-                    if "choose_int" in hyperparam:
-                        value_hyper = int(random.choice(
-                            re.search('\{(.+?)\}', hyperparam).group(1).split(
-                                '|')))
-                        hyper_found = True
+                        if "choose_float" in hyperparam:
+                            value_hyper = float(random.choice(
+                                re.search('\{(.+?)\}', hyperparam).group(1).split(
+                                    '|')))
+                            hyper_found = True
 
-                    if "choose_float" in hyperparam:
-                        value_hyper = float(random.choice(
-                            re.search('\{(.+?)\}', hyperparam).group(1).split(
-                                '|')))
-                        hyper_found = True
+                        line_out = key + '=' + str(value_hyper) + '\n'
 
-                    line_out = key + '=' + str(value_hyper) + '\n'
+                if not hyper_found:
+                    line_out = line
 
-            if not (hyper_found):
-                line_out = line
+                cfg_out.write(line_out)
 
-            cfg_out.write(line_out)
-
-        print('Done %s' % cfg_file_out)
-
-        cfg_out.close()
+            print('Done %s' % cfg_file_out)
