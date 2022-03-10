f5c6d33ce0799f41ba830690161edf7b37f38127
Walter Heymans <walterheymans07@gmail.com>
2022-03-10 07:02:46 +0200

    check_res_dec: Remove whitespace
    Empty lines are trimmed from the end of the file.
    
    Signed-off-by: Walter Heymans <walterheymans07@gmail.com>

--- check_res_dec.sh
+++ check_res_dec.sh
@@ -2,7 +2,3 @@
 for x in $1; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep WER $x/wer_* 2>/dev/null | ./best_wer.sh; done
 for x in $1; do [ -d $x ] && echo $x | grep "${1:-.*}" >/dev/null && grep Sum $x/*score_*/*.sys 2>/dev/null | ./best_wer.sh; done
 exit 0
-
-
-
-
